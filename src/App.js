605d6e2377baa40105ed1fdbbc8a5984dd4a6c03
Adisie Fenta <adisiefenta23@gmail.com>
2023-11-03 13:32:00 +0300

    adding zustand

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import { Route,Routes } from "react-router-dom";
 
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Blog from "./components/Blog";
-
-import ThemeContextProvider from "./contexts/ThemeContext";
-
-import './css/index.css';
 
 const App = () => {
     return ( 
         <div className="container">
-            <ThemeContextProvider>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="blogs" element={<Blog />} />
-                </Routes>
-            </ThemeContextProvider>
+            <h1>App</h1>
         </div>
      );
 }
